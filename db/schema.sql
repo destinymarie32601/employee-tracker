c167c0c185e4831293c63f4c0cee08d74762f22c
destiny32601 <destiny32601@gmail.com>
2023-07-12 22:23:22 -0400

    adding comments

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,16 @@
 DROP DATABASE IF EXISTS employee_db;
-
+--drop database if exists--
 CREATE DATABASE employee_db;
-
+--create database--
 USE employee_db;
-
+--use database--
 CREATE TABLE department(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
+---create department table--
 
+--create table for the role--
 CREATE TABLE role(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
     title VARCHAR(30) NOT NULL,
@@ -17,7 +19,7 @@ CREATE TABLE role(
     FOREIGN KEY (department_id) 
     REFERENCES department (id)
 );
-
+--create employee table---
 CREATE TABLE employee (
     id INT NOT NULL  AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
