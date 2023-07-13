0e5625c3c663f9f9ef40a0472d3dda402f21f9c4
destiny32601 <destiny32601@gmail.com>
2023-07-12 20:11:51 -0400

    added function to insert a new department

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ function start() { //prompt for user
         if(answer.action === 'View all departments') {
             viewAllDepartments()
         } else if (answer.action === 'View all roles') {
-            viewAllROles()
+            viewAllRoles()
         } else if (answer.action === 'View all employees') {
             viewAllEmployees()
         } else if (answer.action === 'Add a department') {
@@ -59,8 +59,40 @@ function viewAllDepartments() {
     connection.query('SELECT * FROM department', function (err, results) {
         console.table(results);
         start();
-
     });
 }
+//view all roles
+
+function viewAllRoles () {
+    connection.query('SELECT * FROM role', function (err,results) {
+        console.table(results);
+        start();
+    });
+}
+//view all employees
+function viewAllEmployees () {
+    connection.query('SELECT * FROM employee JOIN role ON employee.role_id = role.id', function (err,results) {
+        console.table(results);
+        start();
+    });
+}
+//add department
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type:'input',
+            message:'What is the department name?',
+            name: 'department_name',
+        }
+    ])
+    .then((answer) => {
+        connection.query('INSERT INTO department(name) VALUES(?)', answer.department_name, function (err,results) {
+            console.table(results);
+            start();
+        })
+    })
+}
+
+
 
 
